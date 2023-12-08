5f02a25a86a8dc513cb4e56886b48ea323aeb3d3
Tiago Gomes <tiagogomes187@gmail.com>
2023-12-08 01:26:35 -0300

    Ajuste do componente Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,11 +1,23 @@
-import styles from './Footer.module.css'
+import styles from "./Footer.module.css";
 
 function Footer() {
-    return (
-        <footer className={styles.footer}>
-            Feito por Tiago Gomes © 2023
-        </footer>
-    )
+  return (
+    <footer className={styles.footer}>
+        <div>
+      <p>&copy; 2023 - Todos os direitos reservados</p>
+      <nav>
+        <ul>
+          <li>
+            <a href="/sobre">Sobre mim</a>
+          </li>
+          <li>
+            <a href="/contatos">Contatos</a>
+          </li>
+        </ul>
+      </nav>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
